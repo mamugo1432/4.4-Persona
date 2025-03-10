0fe2fea522b8f0ad7a5c20d8cf438e9dfee8bddf
mamugo1432 <mamugo1432@gmail.com>
2025-03-10 12:38:14 +0100

    Añadido el test del objeto que lanza excepcion en el constructor y el test de es dni válido

--- tests/com/modelTest/PersonaTest.java
+++ tests/com/modelTest/PersonaTest.java
@@ -2,6 +2,7 @@ package com.modelTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.model.Persona;
@@ -12,63 +13,39 @@ class PersonaTest {
 	private static Persona persona1;
 	private static Persona persona2;
 	private static Persona persona3;
-	
 
+	
+	@BeforeAll
 	static void crearPersonas() {
+
 		try{
+			
 			persona1 = new Persona();
 			persona2 = new Persona("Manuel",20,'H');
-			persona2 = new Persona("Antonia",30,'M', 80.00, 1.70);
+			persona3 = new Persona("Antonia",30,'M', 80.00, 1.70);
 		}catch(InvalidNumberException e) {
 			System.out.println(e.getLocalizedMessage());
 		}
 
 		}
 
+	@Test
+	 void objetoLanzaExcepcion() {
+		assertThrows(InvalidNumberException.class, () -> new Persona("Miguel",-10,'H'));
+	}
 
 	@Test
 	void testDniValido() {
-		assertEquals(persona1.getDni(), persona1.)
+		assertTrue( persona1.esDniValido(persona1.getDni()));
+		assertFalse( persona2.esDniValido(persona2.getDni()+" "));
+		assertFalse(persona3.esDniValido(persona3.getDni()+"9"));
+	}
+	
+	@Test
+	void calcularIMCTest() {
+		
 	}
 
-	@Test
-	void testSetNombre() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	void testSetEdad() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetSexo() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetPeso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetAltura() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testCalcularIMC() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testEsMayorDeEdad() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testToString() {
-		fail("Not yet implemented");
-	}
 
 }
